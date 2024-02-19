db44fe5d204bae19c625f5e8e66e01e14e7a9b72
Dani Assa <dfassa1@gmail.com>
2024-02-19 11:57:16 -0300

    creando navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,21 +1,64 @@
 import React from "react";
-import { Container, Nav, Navbar, NavDropdown, Button } from 'react-bootstrap';
+import {
+  Container,
+  Nav,
+  Navbar,
+  NavDropdown,
+  Button,
+  Image,
+} from "react-bootstrap";
 import Admin from "../../pages/Admin";
+import {
+  imgLogo,
+  colorLogo,
+  bgColorLogo,
+  linkNav,
+  btnNav,
+} from "./navbar.module.css";
+import { Link, useNavigate } from "react-router-dom";
 
 const NavbarV1 = () => {
+  const navigate = useNavigate();
   return (
-    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">Rolling Beer</Navbar.Brand>
+    <Navbar collapseOnSelect expand="lg" className="">
+      <Container fluid>
+        <Navbar.Brand className={bgColorLogo}>
+          <Link to="/" className="text-decoration-none">
+            <Image
+              className={imgLogo}
+              src="https://i.ibb.co/QJ7DRyp/Logo-Rolling-Beer.png"
+            />
+            <span className={colorLogo}> Rolling Beer</span>
+          </Link>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
+        <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
           <Nav className="me-auto">
-            <Nav.Link href="#features">Acerca de</Nav.Link>
-            <Nav.Link href="#pricing">Menu</Nav.Link>
+            <Button
+              size="sm"
+              onClick={() => navigate("/history")}
+              className={btnNav}
+            >
+              Historia
+            </Button>
+            <Button
+              size="sm"
+              onClick={() => navigate("/login")}
+              className={btnNav}
+            >
+              Iniciar sesión
+            </Button>
+            <Button
+              size="sm"
+              onClick={() => navigate("/register")}
+              className={btnNav}
+            >
+              Registrate
+            </Button>
           </Nav>
-          <Nav>
-            <Admin/>
-          </Nav>
+          {/* <Nav>
+            <Admin />
+          </Nav> */}
         </Navbar.Collapse>
       </Container>
     </Navbar>
