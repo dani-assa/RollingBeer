1bad0a4e99f31eb0bd21359f82fe0799aa5de213
Dani Assa <dfassa1@gmail.com>
2024-02-21 10:46:41 -0300

    agregando estilos y validaciones al login

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,10 @@ import Section from '../components/Section/section.jsx';
 const Home = () => {
   return (
     <div>
-      <img
+      {/* <img
         src="https://th.bing.com/th/id/R.4a2f5c03ce773548a730f809c5d890cb?rik=AdPnKimT8kZcuw&pid=ImgRaw&r=0"
         alt=""
-      />
+      /> */}
       <Section/>
     </div>
   );
